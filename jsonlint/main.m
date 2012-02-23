2e52393392ca637df6c6a7cc424e227b7de15ddc
Sanghyuk Suh <han9kin@gmail.com>
2012-02-23 14:16:28 +0900

    Initial Commit

---
+++ jsonlint/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  jsonlint
+//
+//  Created by 서 상혁 on 12. 2. 23..
+//  Copyright (c) 2012년 NHN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
